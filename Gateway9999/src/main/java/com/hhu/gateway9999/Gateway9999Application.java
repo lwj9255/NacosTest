5ebcecfd8f7ed56a1e5bfc69438279327e57160c
lwj9255 <lwj1059168506@gmail.com>
2025-01-26 23:54:50 +0800

    1.26

---
+++ Gateway9999/src/main/java/com/hhu/gateway9999/Gateway9999Application.java
@@ -0,0 +1,15 @@
+package com.hhu.gateway9999;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Gateway9999Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway9999Application.class, args);
+    }
+
+}
